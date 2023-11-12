2bfef8b95086d25ed83a04c0276575bdb6d3d88e
MilagrosSoberon <msoberon@alu.frlp.utn.edu.ar>
2023-11-12 02:41:19 -0300

    CRUD - Service Transaccion 12/11

---
+++ Controllers/TransaccionController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using backend_milagrofinanciero.Services;
+using backend_milagrofinanciero.Data.BankModels;
+
+namespace backend_milagrofinanciero.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransaccionController: ControllerBase
+    {
+        private readonly TransaccionService _service;
+        public TransaccionController(TransaccionService transaccion)
+        {
+            _service = transaccion;
+        }
+
+        [HttpGet]
+        public IEnumerable<Transaccion> Get()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Transaccion> GetById(int id)
+        {
+            var transaccion = _service.GetById(id);
+        
+            if (transaccion is null)
+                    return NotFound();
+
+            return transaccion;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Transaccion transaccion) 
+        {
+            var newTransaccion = _service.Create(transaccion);
+
+            return CreatedAtAction(nameof(GetById), new { id = newTransaccion.Id }, newTransaccion);
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Update(int id, Transaccion transaccion)
+        {
+
+            if (id != transaccion.Id)
+                return BadRequest();
+
+            var transaccionToUpdate = _service.GetById(id);
+
+            if (transaccionToUpdate is not null)
+            {
+                _service.Update(id,transaccion);
+                return NoContent();
+            }
+            else 
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var transaccionToDelete= _service.GetById(id);
+
+            if (transaccionToDelete is not null)
+            {
+                _service.Delete(id);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Services/TransaccionService.cs
@@ -0,0 +1,65 @@
+﻿using backend_milagrofinanciero.Data;
+using backend_milagrofinanciero.Data.BankModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_milagrofinanciero.Services
+{
+    public class TransaccionService
+    {
+        private readonly MilagrofinancieroG1Context _context;
+
+        public TransaccionService(MilagrofinancieroG1Context context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Transaccion> GetAll()
+        {
+            return _context.Transaccions.ToList();
+        }
+
+        public Transaccion? GetById(int id)
+        {
+            return _context.Transaccions.Find(id);
+        }
+
+        public Transaccion Create(Transaccion newTransaccion)
+        {
+            _context.Transaccions.Add(newTransaccion);
+            _context.SaveChanges();
+
+            return newTransaccion;
+        }
+
+        public void Update(int id, Transaccion transaccion)
+        {
+            var existingTransaccion = GetById(transaccion.Id);
+
+            if (existingTransaccion is not null)
+            {
+                existingTransaccion.Monto = transaccion.Monto;
+                existingTransaccion.NumeroOperacion = transaccion.NumeroOperacion;
+                existingTransaccion.Acreditacion = transaccion.Acreditacion;
+                existingTransaccion.Realizacion = transaccion.Realizacion;
+                existingTransaccion.Motivo = transaccion.Motivo;
+                existingTransaccion.Referencia = transaccion.Referencia;
+                existingTransaccion.CuentaOrigenId = transaccion.CuentaOrigenId;
+                existingTransaccion.CuentaDestinoId = transaccion.CuentaDestinoId;
+                existingTransaccion.TipoTransaccionId = transaccion.TipoTransaccionId;
+
+                _context.SaveChanges();
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            var transaccionToDelete = GetById(id);
+            if (transaccionToDelete is not null)
+            { 
+            _context.Transaccions.Remove(transaccionToDelete);
+            _context.SaveChanges();
+            }
+        }
+    }
+}
