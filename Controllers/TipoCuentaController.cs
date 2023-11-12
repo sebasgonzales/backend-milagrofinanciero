49ff2b4fd78188420cc2f0b115353f366a0b19e2
Nuria Robledo <nuriarobledo0@gmail.com>
2023-11-12 04:02:30 -0300

    CRUD, Service, Async, sin validacion NotFound TipoCuenta 12/11

---
+++ Controllers/TipoCuentaController.cs
@@ -0,0 +1,114 @@
+﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using backend_milagrofinanciero.Services;
+using backend_milagrofinanciero.Data.BankModels;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+
+namespace backend_milagrofinanciero.Controllers
+
+{
+    [ApiController]
+    [Route("[controller]")]
+
+
+
+    public class TipoCuentaController : ControllerBase
+    {
+        private readonly TipoCuentaService _service;
+
+        public TipoCuentaController(TipoCuentaService tipocuenta)
+        {
+            _service = tipocuenta;
+
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<TipoCuenta>> Get()
+        {
+            return await _service.GetAll();
+
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TipoCuenta>> GetById(int id)
+        {
+            var tipocuenta = await _service.GetById(id);
+
+            if (tipocuenta is null)
+                return NotFound();
+
+            return tipocuenta;
+        }
+
+
+        //AGREGAR
+        [HttpPost]
+        public async Task<IActionResult> Create(TipoCuenta tipocuenta)
+        {
+            var newTipoCuenta = await _service.Create(tipocuenta);
+
+
+            return CreatedAtAction(nameof(GetById), new { id = newTipoCuenta.Id }, newTipoCuenta);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, TipoCuenta tipoCuenta)
+        {
+            if (id != tipoCuenta.Id)
+                return BadRequest(new { message = $"El ID = {id} de la URL no coincide con el ID({tipoCuenta.Id}) del cuerpo de la solicitud." });
+
+            var tipoCuentaToUpdate = await _service.GetById(id);
+
+            if (tipoCuentaToUpdate is not null)
+            {
+                await _service.Update(id, tipoCuenta);
+                return NoContent();
+
+            }
+            else
+            {
+                return NotFound();
+
+            }
+
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            var tipoCuentaToDelete = await _service.GetById(id);
+
+            if (tipoCuentaToDelete is not null)
+            {
+                await _service.Delete(id);
+                return Ok();
+
+            }
+            else
+            {
+                return NotFound(new { message = $"El tipo cuenta con ID = {id} no existe." });
+
+            }
+
+        }
+
+        //public NotFoundObjectResult TipoCuentaNotFound(int id)
+        //{
+        //    return NotFound( });
+        //}
+
+
+    }
+
+
+
+
+
+
+}
---
+++ Services/TipoCuentaService.cs
@@ -0,0 +1,66 @@
+﻿using backend_milagrofinanciero.Data;
+using backend_milagrofinanciero.Data.BankModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_milagrofinanciero.Services
+{
+    public class TipoCuentaService
+    {
+
+        private readonly MilagrofinancieroG1Context _context;
+        public TipoCuentaService(MilagrofinancieroG1Context context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<IEnumerable<TipoCuenta>> GetAll()
+        {
+            return await _context.TipoCuenta.ToListAsync();
+
+        }
+
+        public async Task<TipoCuenta?> GetById(int id)
+        {
+            return await _context.TipoCuenta.FindAsync(id);
+        }
+
+        public async Task<TipoCuenta> Create(TipoCuenta newtipoCuenta)
+        {
+            _context.TipoCuenta.Add(newtipoCuenta);
+            await _context.SaveChangesAsync();
+
+            return newtipoCuenta;
+
+        }
+
+        public async Task Update(int id, TipoCuenta tipoCuenta)
+        {
+            var existingtipoCuenta = await GetById(id);
+
+            if (existingtipoCuenta is not null)
+            {
+
+                existingtipoCuenta.Nombre = tipoCuenta.Nombre;
+                existingtipoCuenta.Alta = tipoCuenta.Alta;
+                await _context.SaveChangesAsync();
+            }
+
+        }
+
+        public async Task Delete(int id)
+        {
+            var tipoCuentaToDelete = await GetById(id);
+
+            if (tipoCuentaToDelete is not null)
+            {
+
+                _context.TipoCuenta.Remove(tipoCuentaToDelete);
+                await _context.SaveChangesAsync();
+            }
+
+        }
+
+
+    }
+}
