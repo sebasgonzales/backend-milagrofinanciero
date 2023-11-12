b3539bf90cca8bffe711807e4c4bb89ba4fcd2bd
MilagrosSoberon <msoberon@alu.frlp.utn.edu.ar>
2023-11-12 04:02:36 -0300

    CRUD, Service, Async, SIN validacion Not Found ClienteXCuenta 12/11

---
+++ Controllers/ClienteXCuentaController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using backend_milagrofinanciero.Services;
+using backend_milagrofinanciero.Data.BankModels;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+
+namespace backend_milagrofinanciero.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteXCuentaController : ControllerBase
+    {
+        private readonly ClienteXCuentaService _service;
+        public ClienteXCuentaController(ClienteXCuentaService clienteXCuenta)
+        {
+            _service = clienteXCuenta;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ClienteXCuenta>> Get()
+        {
+            return await _service.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ClienteXCuenta>> GetById(int id)
+        {
+            var clienteXCuenta = await _service.GetById(id);
+
+            if (clienteXCuenta is null)
+                return NotFound();
+
+            return clienteXCuenta;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ClienteXCuenta clienteXCuenta)
+        {
+            var newClienteXCuenta = await _service.Create(clienteXCuenta);
+
+            return CreatedAtAction(nameof(GetById), new { id = newClienteXCuenta.Id }, newClienteXCuenta);
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> Update(int id, ClienteXCuenta clienteXCuenta)
+        {
+
+            if (id != clienteXCuenta.Id)
+                return BadRequest(new { message = $"El ID({id}) de la URL no coincide con el ID({clienteXCuenta.Id}) del cuerpo de la solicitud.  " });
+
+            var clienteXCuentaToUpdate = await _service.GetById(id);
+
+            if (clienteXCuentaToUpdate is not null)
+            {
+                await _service.Update(id, clienteXCuenta);
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var clienteXCuentaToDelete = await _service.GetById(id);
+
+            if (clienteXCuentaToDelete is not null)
+            {
+                await _service.Delete(id);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+       
+    }
+}
---
+++ Services/ClienteXCuentaService.cs
@@ -0,0 +1,60 @@
+﻿using backend_milagrofinanciero.Data;
+using backend_milagrofinanciero.Data.BankModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_milagrofinanciero.Services
+{
+    public class ClienteXCuentaService
+    {
+        private readonly MilagrofinancieroG1Context _context;
+
+        public ClienteXCuentaService(MilagrofinancieroG1Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ClienteXCuenta>> GetAll()
+        {
+            return await _context.ClienteXCuenta.ToListAsync();
+        }
+
+        public async Task<ClienteXCuenta?> GetById(int id)
+        {
+            return await _context.ClienteXCuenta.FindAsync(id);
+        }
+
+        public async Task<ClienteXCuenta> Create(ClienteXCuenta newClienteXCuenta)
+        {
+            _context.ClienteXCuenta.Add(newClienteXCuenta);
+            await _context.SaveChangesAsync();
+
+            return newClienteXCuenta;
+        }
+
+        public async Task Update(int id, ClienteXCuenta clienteXCuenta)
+        {
+            var existingClienteXCuenta = await GetById(clienteXCuenta.Id);
+
+            if (existingClienteXCuenta is not null)
+            {
+                existingClienteXCuenta.Rol = clienteXCuenta.Rol;
+                existingClienteXCuenta.Alta = clienteXCuenta.Alta;
+                
+
+                await _context.SaveChangesAsync();
+            }
+
+        }
+
+        public async Task Delete(int id)
+        {
+            var clienteXCuentaToDelete = await GetById(id);
+            if (clienteXCuentaToDelete is not null)
+            {
+                _context.ClienteXCuenta.Remove(clienteXCuentaToDelete);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
