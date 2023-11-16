0fb1c4f30acf2ba336fe52c535a3a6a86e829b38
MilagrosSoberon <113718442+MilagrosSoberon@users.noreply.github.com>
2023-11-16 11:17:49 -0300

    Revert "Feature/crud localidad"

--- Data/DTOS/request/LocalidadDtoIn.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace backend_milagrofinanciero.Data.DTOS.request
-{
-    public class LocalidadDtoIn
-    {
-        public int Id { get; set; }
-        public string CodigoPostal { get; set; }
-        public int ProvinciaId { get; set; }
-    }
-}
