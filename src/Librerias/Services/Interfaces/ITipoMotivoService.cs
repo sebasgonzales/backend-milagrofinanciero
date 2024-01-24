6ff1fe9069da784ef462eee33c21b35049c07eba
Sebastian Gonzales <sebastian.gt.2002@gmai.com>
2024-01-24 16:43:13 -0300

    Se agregró el Controller, Servicio e Interfaz de TipoMotivo, funcionando

---
+++ src/Librerias/Services/Interfaces/ITipoMotivoService.cs
@@ -0,0 +1,16 @@
+﻿using Core.DTO.request;
+using Core.DTO.response;
+using Data.Models;
+
+namespace Services
+{
+    public interface ITipoMotivoService
+    {
+        Task<TipoMotivo> Create(TipoMotivoDtoIn newTipoMotivoDtoIn);
+        Task Delete(int id);
+        Task<IEnumerable<TipoMotivoDtoOut>> GetAll();
+        Task<TipoMotivo?> GetById(int id);
+        Task<TipoMotivoDtoOut?> GetDtoById(int id);
+        Task Update(int id, TipoMotivoDtoIn tipoMotivo);
+    }
+}
